1014b67709f0c6ace029733b9623ae0437dfa646
Kristijan Petkovski <petkovski0522@gmail.com>
2023-12-14 14:16:25 +0100

    4

--- src/pages/aboutus.tsx
+++ src/pages/aboutus.tsx
@@ -1,7 +1,60 @@
-export default function Aboutus() {
-  return (
-    <>
-      <div>About</div>
-    </>
-  );
+// pages/about.tsx
+import { useState } from "react";
+
+interface AboutData {
+  id: number;
+  storyTitle: string;
+  storyImage: string;
+  storyText: string;
+  workTitle: string;
+  workImage: string;
+  workText: string;
 }
+
+interface AboutProps {
+  aboutData: AboutData;
+}
+
+export async function getStaticProps(): Promise<{ props: AboutProps }> {
+  // Fetch data from db.json or an API
+  const response = await fetch("http://localhost:3001/aboutus");
+  const data: AboutData = await response.json();
+
+  return {
+    props: {
+      aboutData: data,
+    },
+  };
+}
+
+const About: React.FC<AboutProps> = ({ aboutData }) => {
+  const [showStory, setShowStory] = useState<boolean>(true);
+
+  const toggleContent = () => {
+    setShowStory((prev) => !prev);
+  };
+
+  return (
+    <div>
+      <h2>About Us</h2>
+      <button onClick={toggleContent}>About Story</button>
+      <button onClick={toggleContent}>About Work</button>
+
+      {showStory ? (
+        <>
+          <img src={aboutData.storyImage} alt="About Story" />
+          <h3>{aboutData.storyTitle}</h3>
+          <p>{aboutData.storyText}</p>
+        </>
+      ) : (
+        <>
+          <img src={aboutData.workImage} alt="About Work" />
+          <h3>{aboutData.workTitle}</h3>
+          <p>{aboutData.workText}</p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default About;
